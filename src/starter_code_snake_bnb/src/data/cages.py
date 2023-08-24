62a46749e1016b30de769c37ab56e672cf867ec6
frederic-eynaud <frederic.eynaud@gmail.com>
2023-08-24 16:31:48 -0700

    Update cages.py

--- src/starter_code_snake_bnb/src/data/cages.py
+++ src/starter_code_snake_bnb/src/data/cages.py
@@ -10,7 +10,7 @@ class Cage(mongoengine.Document):
     name = mongoengine.StringField(required=True)
     price = mongoengine.FloatField(required=True)
     square_meters = mongoengine.FloatField(required=True)
-    is_carpeted = mongoengine.BooleanField(required=True
+    is_carpeted = mongoengine.BooleanField(required=True)
     has_toy = mongoengine.BooleanField(required=True)
     allow_dangerous_snakes = mongoengine.BooleanField(default=False)
 
